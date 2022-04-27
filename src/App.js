11ad382fd55f49c2caa317a7af1b49b4c7d2a69a
Gaston82 <cabezaartaza@hotmail.com>
2022-04-28 00:54:31 +0200

    add home, detail and searchResult routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Routes, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import ListOfGifs from "./components/listOfGifs";
+import Detail from "./pages/detail";
+import Home from "./pages/home";
+import SearchResult from "./pages/searchResult";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/gif/:keyword" element={<ListOfGifs />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/search/:keyword" element={<SearchResult />} />
+        <Route path="/gif/:id" element={<Detail />} />
       </Routes>
     </div>
   );
