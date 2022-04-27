47a0ec288ecc908d602cc43843c5674b154e619e
Gaston82 <cabezaartaza@hotmail.com>
2022-04-28 01:17:04 +0200

    add search gifs functionallity

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,27 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const POPULAR_GIFS = ["Rambo", "Gandhi", "Pity", "Fight"];
 
 export default function Home() {
+  const [keyword, setKeyword] = useState("");
+  //Averiguar la diferencia con el useLocation
+  let navigate = useNavigate();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    navigate(`/search/${keyword}`);
+    console.log(keyword);
+  };
+
+  const handleChange = (evt) => {
+    setKeyword(evt.target.value);
+  };
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={keyword} onChange={handleChange} />
+      </form>
       <h3> Los Gifs más populares</h3>
       <ul>
         {POPULAR_GIFS.map((popularGif) => (
