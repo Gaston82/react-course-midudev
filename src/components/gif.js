cd485e30d59971f906e80593ed1198b6b2ed8fdf
Gaston82 <cabezaartaza@hotmail.com>
2022-04-28 00:53:19 +0200

    add a  link in Gif component

--- src/components/gif.js
+++ src/components/gif.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Gif({ id, title, url }) {
   return (
     <div>
-      <h4>{title}</h4>
-      <img src={url} alt={title} />
+      <Link to={`/gif/${id}`}>
+        <h4>{title}</h4>
+        <img src={url} alt={title} />
+      </Link>
     </div>
   );
 }
